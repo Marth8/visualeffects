b80c9b72eb943d322f82115bdabeb8d33b7463bc
Marth8 <nicolai.froehlig@gmail.com>
2018-11-20 18:33:58 +0100

    trying new sth.

--- Uebung1Uhr/index.js
+++ Uebung1Uhr/index.js
@@ -3,6 +3,7 @@ const gl = canvas.getContext("webgl");
 const vsSourceString =
     `
     attribute vec3 aPosition;
+    uniform mat4 uRotate;
     void main() { 
         gl_PointSize = 10.0;
         gl_Position = vec4(aPosition, 1.0);
@@ -18,6 +19,7 @@ const fsSourceString =
     void main() {
         gl_FragColor = vec4(uColor, 1.0);
     }`;
+let squareRotation = 0.0;
 
 main();
 
@@ -32,76 +34,97 @@ function main() {
 
     let housePositions = new Float32Array(
         [
-            0.4, -0.6 ,
-            -0.4, 0   ,
-            0.4 , 0   ,
+            0.01, 0 ,
+            -0.01, 0.2   ,
+            0.01 , 0.2   ,
 
-            -0.4, -0.6,
-            0.4 , -0.6,
-            -0.4, 0
+            -0.01, 0,
+            0.01 , 0,
+            -0.01, 0.2
         ]
     );
 
     let roofPositions = new Float32Array(
         [
-            -0.5, 0   ,
-            0   , 0.5 ,
-            0.5 , 0
+            -0.05, 0.2   ,
+            0 , 0.25 ,
+            0.05 , 0.2
         ]
     );
 
-    // Bei n-verschiedenen Shadern => n-mal useProgram
-    drawObject(housePositions, 2, 6, vsSourceString, fsSourceString, new Float32Array([1.0, 0.0, 1.0]));
-    drawObject(roofPositions, 2, 3, vsSourceString, fsSourceString, new Float32Array([0.0, 1.0, 0.0]));
-}
-
-function drawObject(positions, dimension, vertexCount, vsSourceString, fsSourceString, colorArray)
-{
     const vertexShader = createVertexShader(vsSourceString);
     const fragmentShader = createFragmentShader(fsSourceString);
     const shaderProgram = initShader(vertexShader, fragmentShader);
-    const buffers = initBuffer(shaderProgram, positions, colorArray);
     const programInfo = {
+        shaderProgram: shaderProgram,
         attributeLocations: {
             aPosition: gl.getAttribLocation(shaderProgram, "aPosition")
         },
         uniformLocations: {
             uColor: gl.getUniformLocation(shaderProgram, "uColor"),
+            uRotate: gl.getUniformLocation(shaderProgram, "uRotate")
         }
     };
+    /*
+    // Draw the scene repeatedly
+    function render(now) {
+        now *= 0.001;  // convert to seconds
+        const deltaTime = now - then;
+        then = now;
+
+        drawScene(gl, programInfo, buffers, deltaTime);
+
+        requestAnimationFrame(render);
+    }
+    requestAnimationFrame(render);*/
+
+    // Bei n-verschiedenen Shadern => n-mal useProgram
+    //drawObject(programInfo, housePositions, 2, 6, new Float32Array([1.0, 0.0, 1.0]));
+    //drawObject(programInfo, roofPositions, 2, 3, new Float32Array([0.0, 1.0, 0.0]));
+    drawScene(programInfo, housePositions, roofPositions, 0);
+
+    // Aufräumen
+    gl.detachShader(programInfo.shaderProgram, vertexShader);
+    gl.detachShader(programInfo.shaderProgram, fragmentShader);
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+    gl.deleteProgram(shaderProgram);
+}
+
+//
+// Draw the scene.
+//
+function drawScene(programInfo, housePositions, roofPositions, deltaTime) {
+    this.resetCanvas();
+
+    // Bei n-verschiedenen Shadern => n-mal useProgram
+    drawObject(programInfo, housePositions, 2, 6, new Float32Array([1.0, 0.0, 1.0]));
+    drawObject(programInfo, roofPositions, 2, 3, new Float32Array([0.0, 1.0, 0.0]));
+
+    squareRotation += deltaTime;
+}
+
+function drawObject(programInfo, positions, dimension, vertexCount, colorArray)
+{
+    const buffers = initBuffer(programInfo.shaderProgram, positions, colorArray);
 
     // Den Position-Buffer binden und verbinden
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(programInfo.attributeLocations.position, dimension, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(programInfo.attributeLocations.position);
 
-    // Den Color-Buffer binden und verbinden
-    //gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-    //gl.vertexAttribPointer(programInfo.attributeLocations.color, colorArray.length, gl.FLOAT, false, 0, 0);
-    //gl.enableVertexAttribArray(programInfo.attributeLocations.color);
-
-    // Das Uniform binden und verbinden
-    //gl.bindBuffer(gl.ARRAY_BUFFER, buffers.color);
-    //gl.vertexAttribPointer(programInfo.uniformLocations.uColor, 3, gl.FLOAT, false, 0, 0);
-    //gl.enableVertexAttribArray(programInfo.uniformLocations.uColor);
+    const rotationMatrix = mat4.create();
 
     // Bei n-verschiedenen Shadern => n-mal useProgram
-    gl.useProgram(shaderProgram);
+    gl.useProgram(programInfo.shaderProgram);
     gl.uniform3fv(programInfo.uniformLocations.uColor, colorArray);
-    //gl.uniform3f(color, colorArray[0], colorArray[1], colorArray[2]);
+    gl.uniform4fv(programInfo.uniformLocations.uRotate, rotationMatrix);
 
     // offset is 0, with 1 element, TRIANGLE STRIP
     gl.drawArrays(gl.TRIANGLES, 0, vertexCount);
 
-    gl.disableVertexAttribArray(buffers.color);
-    gl.disableVertexAttribArray(buffers.position);
-
-    // Aufräumen
-    gl.detachShader(shaderProgram, vertexShader);
-    gl.detachShader(shaderProgram, fragmentShader);
-    gl.deleteShader(vertexShader);
-    gl.deleteShader(fragmentShader);
-    gl.deleteProgram(shaderProgram);
+    gl.disableVertexAttribArray(programInfo.attributeLocations.position);
+    gl.disableVertexAttribArray(programInfo.uniformLocations.uColor);
 }
 
 function initBuffer(shaderProgram, positions, color)
